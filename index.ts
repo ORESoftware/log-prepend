e813a25d34bda21371ab6d14d263a81bb6ae58ad
Olegzandr VD <alex@oresoftware.com>
2017-09-10 14:48:38 -0700

    pdev:set

--- index.ts
+++ index.ts
@@ -6,19 +6,8 @@ export const lp = function (str: string, strm: Writable) {
 
     const hasNonWhitespace = args.some(function (a) {
       let str = String(a);
-      // console.log('str => ', str);
-      // let one = str.length > 0;
-      // let two = /\S/g.test(str);
-      // console.log('one => ', one);
-      // console.log('two => ', two);
-      // return str.length > 0 && /\S/g.test(str);
-
-      // return one && two && true;
       return str.length > 0 && /\S/g.test(str);
     });
-
-    // console.log('args => ', args);
-    // console.log('hasNonWhitespace => ', hasNonWhitespace);
 
     if (hasNonWhitespace) {
       strm.write(str);
